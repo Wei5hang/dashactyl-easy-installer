6957582cd1104d87b6bd36e486000cbc541ef645
Wei5hang <86553289+Wei5hang@users.noreply.github.com>
2021-06-28 13:00:54 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,10 +3,10 @@
 set -e
 #############################################################################
 #                                                                           #
-# Github Project 'dashactyl-installer'                                      #
-# By Dannni#0058                                                        #
+# By Dannni#0058                                                            #
 #                                                                           #
 #############################################################################
+
    clear
 
     echo "Dashactyl Install Script"
@@ -28,6 +28,12 @@ set -e
     curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
     sudo apt -y install nodejs
     sudo apt -y  install gcc g++ make
+    echo "-------------------------------------------------------"
+    echo "NodeJS Install Completed!"
+    echo "-------------------------------------------------------"
+
+    sleep 1
+
     echo "------------------------------------------------------"
     echo "Starting Dependercy install."
     echo "------------------------------------------------------"
@@ -36,12 +42,56 @@ set -e
     echo "Dependercy Install Completed!"
     echo "-------------------------------------------------------"
 
+    sleep 1
+
     echo "-------------------------------------------------------"
-    echo "Starting File install."
+    echo "Starting Dashactyl install."
     echo "-------------------------------------------------------"
     git clone https://github.com/real2two/dashactyl.git
     cd dashactyl
     sudo npm install
     echo "-------------------------------------------------------"
-    echo "Dashactyl File Install Completed!"
+    echo "Dashactyl Install Completed!"
+    echo "-------------------------------------------------------"
+
+    sleep 1
+
+    echo "-------------------------------------------------------"
+    echo "Starting Settings Configuration."
+    echo "Read the Docs for further infomration."
+    echo "https://cutt.ly/omyptWe"
+    echo "-------------------------------------------------------"
+
+    sleep 1
+
+    cd dashactyl
+    file=settings.json
+
+    echo "What is the web port? [80] (The port Dashactyl will run on)"
+    read WEBPORT
+    echo "What is the pterodactyl URL? (https://panel.domain.com)"
+    read PTERODACTYL_DOMAIN
+    echo "What is the pterodactyl API key? (Perms: https://cutt.ly/dmypP54)"
+    read PTERODACTYL_KEY
+    echo "What is the Discord Oauth2 ID?"
+    read DOAUTH_ID
+    echo "What is the Discord Oauth2 Secret?"
+    read DOAUTH_SECRET
+    echo "What is the Discord Oauth2 Link?"
+    read DOAUTH_LINK
+    echo "What is the Callback path? [callback]" 
+    read DOAUTH_CALLBACKPATH
+
+    sed -i 
+    -e 's/"port":.*/"port": '$WEBPORT',/' 
+    -e 's/"domain":.*/"domain": "'$PTERODACTYL_DOMAIN'",/' 
+    -e 's/"key":.*/"key": "'$PTERODACTYL_KEY'"/' 
+    -e 's/"id":.*/"id": "'$DOAUTH_ID'",/' 
+    -e 's/"link":.*/"link": "'$DOAUTH_LINK'",/' 
+    -e 's/"path":.*/"path": "'$DOAUTH_CALLBACKPATH'",/' 
+    -e '0,/"secret":.*/! {0,/"secret":.*/ s/"secret":.*/"secret": "'$DOAUTH_SECRET'",/}' 
+    $file
+
+    echo "-------------------------------------------------------"
+    echo "Dashactyl installation Completed!"
     echo "-------------------------------------------------------"
