99f0c311356284d344f127b793f260e661b03599
Wei5hang <weishang.lu@colegiovascodagama.eu>
2021-06-28 13:05:36 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -22,36 +22,12 @@ set -e
     echo "-------------------------------------------------------"
     fi
 
-install_options(){
-    echo "Select your installation option:"
-    echo "[1] - Full Fresh Dashactyl Install (Dependercies, Files)"
-    echo "[2] - Install the Dependercies."
-    echo "[3] - Install the Files."
-    echo "-------------------------------------------------------"
-    read choice
-    case $choice in
-        1 ) installoption=1
-            dependercy_install
-            file_install
-            ;;
-        2 ) installoption=2
-            dependercy_install
-            ;;
-        3 ) installoption=3
-            file_install
-            ;;
-        * ) output "You did not enter a valid selection."
-            install_options
-    esac
-}
+    sleep 6
 
-dependercy_install() {
     echo "------------------------------------------------------"
     echo "Starting NodeJS install."
     echo "------------------------------------------------------"
     sudo apt update -y && sudo apt upgrade -y
-
-    sudo apt update
     sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
     curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
     sudo apt -y install nodejs
@@ -60,12 +36,10 @@ dependercy_install() {
     echo "Starting Dependercy install."
     echo "------------------------------------------------------"
     sudo apt install git -y
-    apt install nginx -y
     echo "-------------------------------------------------------"
     echo "Dependercy Install Completed!"
     echo "-------------------------------------------------------"
-}
-file_install() {
+
     echo "-------------------------------------------------------"
     echo "Starting File install."
     echo "-------------------------------------------------------"
@@ -75,6 +49,3 @@ file_install() {
     echo "-------------------------------------------------------"
     echo "Dashactyl File Install Completed!"
     echo "-------------------------------------------------------"
-}
-install_options
-
