a76581cebab2dcc81b2b2207c3c0fff657e04e8e
Wei5hang <weishang.lu@colegiovascodagama.eu>
2021-06-28 12:55:36 +0800

    Test

--- test.sh
+++ test.sh
@@ -22,36 +22,12 @@ set -e
     echo "-------------------------------------------------------"
     fi
 
-install_options(){
-    echo "Select your installation option:"
-    echo "[1] - Full Fresh Dashactyl Install (Dependercies, Files)"
-    echo "[2] - Install the Dependercies."
-    echo "[3] - Install the Files."
-    echo "-------------------------------------------------------"
-    read choice
-    case $choice in
-        1 ) installoption=1
-            dependercy_install
-            file_install
-            ;;
-        2 ) installoption=2
-            dependercy_install
-            ;;
-        3 ) installoption=3
-            file_install
-            ;;
-        * ) output "You did not enter a valid selection."
-            install_options
-    esac
-}
+    sleep 5
 
-dependercy_install() {
     echo "------------------------------------------------------"
     echo "Starting NodeJS install."
     echo "------------------------------------------------------"
     sudo apt update -y && sudo apt upgrade -y
-
-    sudo apt update
     sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
     curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
     sudo apt -y install nodejs
@@ -64,8 +40,7 @@ dependercy_install() {
     echo "-------------------------------------------------------"
     echo "Dependercy Install Completed!"
     echo "-------------------------------------------------------"
-}
-file_install() {
+
     echo "-------------------------------------------------------"
     echo "Starting File install."
     echo "-------------------------------------------------------"
@@ -75,6 +50,3 @@ file_install() {
     echo "-------------------------------------------------------"
     echo "Dashactyl File Install Completed!"
     echo "-------------------------------------------------------"
-}
-install_options
-
