11a85494decebc5f9d45051b05f28a800b51096e
Wei5hang <86553289+Wei5hang@users.noreply.github.com>
2021-08-06 17:37:18 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,14 @@
-touch test.txt
+    cd ~
+    sudo apt update -y && sudo apt upgrade -y
+    sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+    curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+    sudo apt -y install nodejs
+    sudo apt -y  install gcc g++ make
+    sudo apt install git -y
+
+    sleep 1
+
+    git clone https://github.com/real2two/dashactyl.git
+    cd dashactyl
+    sudo npm install
+
